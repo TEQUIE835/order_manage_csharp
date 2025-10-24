fc21d7f76af34515165eb83247cf9ae2eb5636dd
TEQUIE835 <david123orjuela@gmail.com>
2025-10-24 12:37:14 -0500

    First Upload

--- orderManage.Application/obj/Debug/net8.0/orderManage.Application.AssemblyInfo.cs
+++ orderManage.Application/obj/Debug/net8.0/orderManage.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("orderManage.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d68b4a0cf6fb466dd578386a91621fb6df66378")]
 [assembly: System.Reflection.AssemblyProductAttribute("orderManage.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("orderManage.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
